cfee34c9f65ade7990ec44f790fee359fb26ef2c
tarotene <tarotene.dev@gmail.com>
2024-09-04 01:07:34 +0900

    Rewrite error-handling and other stuffs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 #![no_std]
 
-use embedded_hal::i2c;
 use bitfield_struct::bitfield;
+use embedded_hal::blocking::i2c;
+
+#[derive(Debug, PartialEq)]
+pub enum Sts32_33DisError<E> {
+    I2C(E),
+}
 
 #[bitfield(u16, order = Msb)]
 pub struct StatusRegister {
@@ -10,47 +15,48 @@ pub struct StatusRegister {
     #[bits(1)]
     __: u8,
     #[bits(1, access = RO)]
-    heater: u8,
+    pub heater: u8,
     #[bits(2)]
     __: u8,
     #[bits(1, access = RO)]
-    tracking_alert: u8,
+    pub tracking_alert: u8,
     #[bits(5)]
     __: u8,
     #[bits(1, access = RO)]
-    system_reset_detected: u8,
+    pub system_reset_detected: u8,
     #[bits(2)]
     __: u8,
     #[bits(1, access = RO)]
-    command: u8,
+    pub command: u8,
     #[bits(1, access = RO)]
-    write_data_check_sum: u8,
+    pub write_data_check_sum: u8,
 }
 
-pub struct Sts32_33Dis<I2C>
-where I2C: i2c::I2c {
+pub struct Sts32_33DisDriver<I2C>
+where
+    I2C: i2c::Read + i2c::Write + i2c::WriteRead,
+{
     i2c: I2C,
     address: u8,
 }
 
-impl<I2C> Sts32_33Dis<I2C>
-where I2C: i2c::I2c {
+impl<I2C, E> Sts32_33DisDriver<I2C>
+where
+    I2C: i2c::Read<Error = E> + i2c::Write<Error = E> + i2c::WriteRead<Error = E>,
+{
     pub fn new(i2c: I2C, address: u8) -> Self {
-        Sts32_33Dis {
-            i2c,
-            address,
-        }
+        Sts32_33DisDriver { i2c, address }
     }
 
-    pub fn status_register(&mut self) -> Result<StatusRegister, I2C::Error> {
+    pub fn status_register(&mut self) -> Result<StatusRegister, Sts32_33DisError<E>> {
         self.read_status_register()
     }
 
-    fn read_status_register(&mut self) -> Result<StatusRegister, I2C::Error> {
+    fn read_status_register(&mut self) -> Result<StatusRegister, Sts32_33DisError<E>> {
         let mut data = [0; 3];
 
-        self.i2c.write(self.address, &[0xF3, 0x2D])?;
-        self.i2c.read(self.address, &mut data)?;
+        self.i2c.write(self.address, &[0xF3, 0x2D]).map_err(|e| Sts32_33DisError::I2C(e))?;
+        self.i2c.read(self.address, &mut data).map_err(|e| Sts32_33DisError::I2C(e))?;
 
         Ok(StatusRegister::from(u16::from_be_bytes([data[0], data[1]])))
     }
